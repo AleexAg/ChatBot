6d33beeab8cbecc6860273e82ac3683572238b0a
Alex <aguirre.alex.na@gmail.com>
2025-02-01 01:00:25 -0300

    fix error

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 import streamlit as st
 from langchain_openai import ChatOpenAI
 
-llModel = ChatOpenAI(model="gpt-4o", temperature=0, api_key="")
+llModel = ChatOpenAI(model="gpt-3.5-turbo", temperature=0, api_key=" ")
 
 
 st.title('CHATBOT GPT-4')
-messages = [("System", "Eres un chatbot AI de utilidad, tu nombre es botin, habla como si fueras un humano")]
+messages = [("system", """Eres un chatbot AI de utilidad, tu nombre es botin, habla como si fueras un humano""")]
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-for messages in st.session_state.messages:
-    with st.chat_message(messages["role"]):
-        st.markdown(messages["content"])
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
 if prompt := st.chat_input("En que puedo ayudarte? "):
     st.chat_message("user").markdown(prompt)
     st.session_state.messages.append({"role": "user", "content": prompt})
-    messages.append("human", prompt)
+    messages.append(["human", prompt])
 
     response = llModel.invoke(messages).content
 
